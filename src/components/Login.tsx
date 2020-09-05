ebc31c13a3fcadfa5393ee0ceb16fd21ffe5c422
Mohamed Najiub <mohamednajiub@gmail.com>
2020-09-05 19:18:53 +0200

    use login interfaces and use login response

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,18 +4,28 @@ import { Formik, Form } from 'formik';
 
 import * as Yup from 'yup';
 import InputField from './InputField/InputField';
+import { LoginSubmit, LoginResponse } from '../Interfaces/Auth/Login';
 
 import Axios from '../utils/Axios';
 
-const Login: React.FC = () => {
-	const on_submit = (email: string, password: string, setSubmitting) => {
+const Login: React.FC = (props: any) => {
+	const on_submit: LoginSubmit = (email, password, setSubmitting) => {
 		const loginData = {
 			email: email,
 			password: password,
 		};
 		Axios.post('/login', loginData)
-			.then((response) => {
+			.then((response: LoginResponse) => {
 				console.log(response);
+				let expirationDate: Date = new Date(
+					new Date().getTime() + response.data.expirationTime * 60 * 1000
+				);
+
+				// set token and expiration date in local storage
+				localStorage.setItem('token', response.data.token);
+				localStorage.setItem('expirationDate', `${expirationDate}`);
+				localStorage.setItem('userID', `${response.data.userId}`);
+				localStorage.setItem('type', `${response.data.type}`);
 				setSubmitting(false);
 			})
 			.catch((error) => {
@@ -33,7 +43,7 @@ const Login: React.FC = () => {
 			.required('No Password Provided')
 			.min(8, 'Password is too short it must be at least 8 characters or longer')
 			.matches(
-				/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,20}/,
+				/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}/,
 				'Your password must have numbers, capital letters, small letters and special characters '
 			),
 	});
