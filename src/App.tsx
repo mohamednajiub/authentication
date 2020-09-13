70098a2f279f75267473899b0444836fb0a48c95
Mohamed Najiub <mohamednajiub@gmail.com>
2020-09-13 20:15:01 +0200

    add app header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Login from './components/Login';
+import AppHeader from './components/AppHeader/AppHeader';
 
-function App() {
+import { Route, Switch } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+	container: {},
+});
+const App = () => {
+	const classes = useStyles();
 	return (
-		<div className='App'>
-			<Login />
+		<div className={classes.container}>
+			<AppHeader />
+			<Switch>
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/Register' render={(props) => <h1>hi Register</h1>} />
+			</Switch>
 		</div>
 	);
-}
+};
 
 export default App;
---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import Drawer from '@material-ui/core/Drawer';
+
+import MainHorizontalLogo from '../../images/logos/white-logo-cropped.png'
+
+import { MenuItem, Link, Hidden, Container } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		title: {
+			flexGrow: 1,
+		},
+		headerWrapper: {
+			display: 'flex',
+			justifyContent: 'space-between',
+			alignItems: 'center'
+		},
+		menu: {
+			display: 'flex',
+			flexDirection: 'row',
+		},
+		brand: {
+			display: 'block',
+		},
+		brandLogo: {
+			display: 'block',
+			height: '40px',
+			width: 'auto',
+			objectFit: 'cover'
+		}
+	})
+);
+export default function ButtonAppBar() {
+	const classes = useStyles();
+	const [open_drawer, set_drawer] = useState(false);
+
+	const toggleDrawer = () => {
+		set_drawer(!open_drawer)
+	}
+
+	return (
+		<AppBar position='static'>
+			<Container disableGutters>
+				<Toolbar className={classes.headerWrapper}>
+					<Link href="https://ieeehsb.org" className={classes.brand}>
+						<img src={MainHorizontalLogo} alt="IEEE Helwan Student Branch" className={classes.brandLogo} />
+					</Link>
+
+					<Hidden only={['xs', 'sm']}>
+						<nav className={classes.menu}>
+							<MenuItem component="a" href="https://ieeehsb.org">
+								Home
+							</MenuItem>
+							<MenuItem component="a" href="/login">
+								Login
+							</MenuItem>
+							<MenuItem component="a" href="/register">
+								Registeration
+							</MenuItem>
+						</nav>
+					</Hidden>
+
+					<Hidden mdUp>
+						<IconButton
+							edge='start'
+							color='inherit'
+							aria-label='menu'
+							onClick={toggleDrawer}
+						>
+							<MenuIcon />
+						</IconButton>
+					</Hidden>
+				</Toolbar>
+				<Hidden mdUp>
+					<Drawer anchor="left" open={open_drawer} onClose={toggleDrawer}>
+						<nav>
+							<MenuItem component="a" href="https://ieeehsb.org">
+								Home
+							</MenuItem>
+							<MenuItem component="a" href="/login">
+								Login
+							</MenuItem>
+							<MenuItem component="a" href="/register">
+								Registeration
+							</MenuItem>
+						</nav>
+					</Drawer>
+				</Hidden>
+			</Container>
+		</AppBar>
+	);
+}
